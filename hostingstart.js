506e0722de09786a377d2f3f0cb244cb729d2621
Ceascai Andra Maria <maceascai@gmail.com>
2023-11-28 17:04:59 +0200

    12

---
+++ hostingstart.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import request from 'request'
+
+const router = express.Router()
+
+router.get('/', async function(req, res) {
+ 
+    var token = req.headers["x-ms-token-aad-access-token"]
+     
+        getstuff(token).then(response => {
+          console.log(response)
+         res.json(response)
+         })
+         .catch(error => {
+             res.send(error)
+         })
+       
+     });

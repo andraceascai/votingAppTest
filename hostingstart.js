913865077828581e01e649ac05b20bb2c6f02d9a
Ceascai Andra Maria <maceascai@gmail.com>
2023-11-28 17:13:29 +0200

    14

--- hostingstart.js
+++ hostingstart.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import request from 'request'
 
-const router = express.Router()
+// const router = express.Router()
 
-console.log(router)
+console.log("router")
 
-router.get('/', async function(req, res) {
+// router.get('/', async function(req, res) {
  
-    var token = req.headers["x-ms-token-aad-access-token"]
+//     var token = req.headers["x-ms-token-aad-access-token"]
      
-        getstuff(token).then(response => {
-          console.log(response)
-         res.json(response)
-         })
-         .catch(error => {
-             res.send(error)
-         })
+//         getstuff(token).then(response => {
+//           console.log(response)
+//          res.json(response)
+//          })
+//          .catch(error => {
+//              res.send(error)
+//          })
        
-     });
+//      });

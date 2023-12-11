e664537cd66761e1052bfa0b343cad94e8bef95f
Ceascai Andra Maria <maceascai@gmail.com>
2023-12-11 17:37:21 +0200

    dzeu

--- hostingstart.js
+++ hostingstart.js
@@ -1,9 +1,9 @@
 // import express from 'express';
-// import request from 'request'
+// // import request from 'request'
 
 // const router = express.Router()
 
-console.log("router and friends")
+// // console.log("router and friends")
 
 // router.get('/', async function(req, res) {
  
@@ -18,3 +18,52 @@ console.log("router and friends")
 //          })
        
 //      });
+import express, { Router } from 'express';
+import request from 'request';
+const router =Router()
+
+var app = express()
+router.get('/', async function(req, res) {
+ 
+ var token = req.headers["x-ms-token-aad-access-token"]
+  
+     getstuff(token).then(response => {
+       console.log(response)
+      res.json(response)
+      })
+      .catch(error => {
+          res.send(error)
+      })
+    
+  });
+  
+  var getstuff =  async function (token) {
+    
+    console.log("getting stuff");
+    
+   
+var options = {
+  'method': 'GET',
+  'url': 'https://voting-app-test.azurewebsites.net/.auth/me',
+
+  'headers': {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+ token
+  }
+};
+console.log(options)
+return new Promise(resolve => {
+  request( options,  function (error, response, body) {
+     if(!error){
+       console.log(body)
+      resolve(JSON.parse(body));
+
+
+     }
+        
+  });
+  
+ 
+});
+
+  }

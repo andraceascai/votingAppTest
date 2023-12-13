8cf37e62fae2668416a7544071f12cf72e337b68
Ceascai Andra Maria <maceascai@gmail.com>
2023-12-13 13:57:28 +0200

    z

--- hostingstart.js
+++ hostingstart.js
@@ -1,69 +1,71 @@
-// import express from 'express';
-// // import request from 'request'
+// // import express from 'express';
+// // // import request from 'request'
 
-// const router = express.Router()
+// // const router = express.Router()
 
-// // console.log("router and friends")
+// // // console.log("router and friends")
 
+// // router.get('/', async function(req, res) {
+ 
+// //     var token = req.headers["x-ms-token-aad-access-token"]
+     
+// //         getstuff(token).then(response => {
+// //           console.log(response)
+// //          res.json(response)
+// //          })
+// //          .catch(error => {
+// //              res.send(error)
+// //          })
+       
+// //      });
+// import express, { Router } from 'express';
+// import request from 'request';
+// const router =Router()
+
+// var app = express()
 // router.get('/', async function(req, res) {
  
-//     var token = req.headers["x-ms-token-aad-access-token"]
-     
-//         getstuff(token).then(response => {
-//           console.log(response)
-//          res.json(response)
-//          })
-//          .catch(error => {
-//              res.send(error)
-//          })
-       
-//      });
-import express, { Router } from 'express';
-import request from 'request';
-const router =Router()
-
-var app = express()
-router.get('/', async function(req, res) {
- 
- var token = req.headers["x-ms-token-aad-access-token"]
+//  var token = req.headers["x-ms-token-aad-access-token"]
   
-     getstuff(token).then(response => {
-       console.log(response)
-      res.json(response)
-      })
-      .catch(error => {
-          res.send(error)
-      })
+//      getstuff(token).then(response => {
+//        console.log(response)
+//       res.json(response)
+//       })
+//       .catch(error => {
+//           res.send(error)
+//       })
     
-  });
+//   });
   
-  var getstuff =  async function (token) {
+//   var getstuff =  async function (token) {
     
-    console.log("getting stuff");
+//     console.log("getting stuff");
     
    
-var options = {
-  'method': 'GET',
-  'url': 'https://voting-app-test.azurewebsites.net/.auth/me',
-
-  'headers': {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer '+ token
-  }
-};
-console.log(options)
-return new Promise(resolve => {
-  request( options,  function (error, response, body) {
-     if(!error){
-       console.log(body)
-      resolve(JSON.parse(body));
-
-
-     }
+// var options = {
+//   'method': 'GET',
+//   'url': 'https://voting-app-test.azurewebsites.net/.auth/me',
+// };
+// console.log(options)
+// return new Promise(resolve => {
+//   request( options,  function (error, response, body) {
+//      if(!error){
+//        console.log(body)
+//       resolve(JSON.parse(body));
+//      }else console.log('error')
         
-  });
-  
- 
-});
+//   });
+// });
 
+//   }
+
+async function getUserInfo() {
+    const response = await fetch('https://voting-app-test.azurewebsites.net/.auth/me');
+    const payload = response;
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
   }
+  
+  (async () => {
+  console.log(await getUserInfo());
+  })();
